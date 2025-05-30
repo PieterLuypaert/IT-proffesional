d567da21c62bef157c48270ec1aa6833443e30a8
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-05-30 13:10:09 +0200

    Add webpack.mix.js for asset compilation setup

---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+let mix = require("laravel-mix");
+
+mix.js("src/scripts/main.js", "build").setPublicPath("build");
+
+
+mix.postCss("src/styles/main.css", "build", [
+    require("postcss-nested"),
+  ]);
